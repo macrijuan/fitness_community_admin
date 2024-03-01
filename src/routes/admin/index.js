9376935f14abb1a0e6bdd10eecc9621568317e23
macrijuan <amacri162013@yahoo.com>
2024-03-01 12:46:41 -0300

    change postAdmin.js name to post_admin.js

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const postAdmin = require("./postAdmin");
+const postAdmin = require("./post_admin");
 
 router.use("/admin", postAdmin);
 
