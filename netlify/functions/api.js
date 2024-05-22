a4602344f689681b1353345de23f07fb97af34e6
macrijuan <amacri162013@yahoo.com>
2024-05-22 00:35:53 -0300

    UPDATE netlify\functions\api.js --> end rate limit handler process if limit has been reached

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -33,6 +33,7 @@ server.use(
         return res.status( 429 ).json( req_limit );
       };
       console.log('Request limit reached for IP:', req.ip);
+      return;
     },
     keyGenerator: (req) => {
       req.ip = req.headers['x-forwarded-for']?.split(',')[0].trim();
