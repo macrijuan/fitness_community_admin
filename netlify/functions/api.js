000b27713c2119d83de5fd5211b24b2c307d09fd
macrijuan <amacri162013@yahoo.com>
2024-05-29 23:55:43 -0300

    console log after redis lab succesful conection

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -66,6 +66,9 @@ const redisClient = createClient({
 redisClient.on('error', (err) => {
   console.error('Redis error:', err);
 });
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
 
 redisClient.connect().catch(console.error);
 
