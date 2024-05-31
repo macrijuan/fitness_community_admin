a464b82328265aa999f898b584442ec71596fba5
macrijuan <amacri162013@yahoo.com>
2024-05-31 02:10:09 -0300

    set session cookie back to unable from the frontend

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -140,7 +140,7 @@ server.use(
     cookie: {
       maxAge: 7200000,//2hs
       secure: true,//CHANGE IN DEVELOPMENT TO false!!!!!!!!!!!!!!!!!!
-      httpOnly: false,
+      httpOnly: true,
       sameSite: 'strict',
       }
   })
